61f91a2eafdbe429e0b4b07c1f3dabfc08001da0
jmr422 <jmrk422@gmail.com>
2021-05-24 11:48:11 +0530

    first commit by lead

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Demo for latest git tutorial");
+		int x=10;
+		System.out.println(x);
+		
+	}
+
+}
